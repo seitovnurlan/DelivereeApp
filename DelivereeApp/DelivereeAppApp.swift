766867492e9b015a61bf78c6bd38e8dd646e981a
Nurlan Seitov <seitov@gmail.com>
2023-04-14 16:35:25 +0600

    Initial Commit

---
+++ DelivereeApp/DelivereeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelivereeAppApp.swift
+//  DelivereeApp
+//
+//  Created by Nurlan Seitov on 14/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DelivereeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
